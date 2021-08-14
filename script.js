462237ca51c13ac13d94debae999f272a89ac31f
Steve <asapcrafter@gmail.com>
2021-08-13 20:03:05 -0700

    api works

---
+++ script.js
@@ -0,0 +1,39 @@
+const userLocation = 95376
+let weatherData
+
+
+const getData = async function getWeatherData(userLocation) {
+    try {
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=6a0e226f90fe438f90b02617211408&q=${userLocation}&aqi=no`);
+        const data = await response.json();
+        // weatherData = data;
+        processData(data);
+    } catch (err) {
+        console.log(err);
+    };
+}
+
+const processData = function processWeatherData(data) {
+    location = 
+    temperature = data.current.temp_f
+    
+}
+
+//Debugging stuff
+console.log(getData(95376))
+
+getData(95376)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
